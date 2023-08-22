0e50130105b56149691212ef5c82fe8f9496d315
boyun <boyunk@uci.edu>
2023-08-22 13:57:17 -0700

    D29: dictionary api practice(something's wrong)

--- module4/Practice/main.py
+++ module4/Practice/main.py
@@ -11,13 +11,12 @@ app = Flask("__name__")
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<word>")
-def about(word):
-    #df = pd.read_csv("")
-    definition = word.upper()
-    return{"definition":definition,
-           "word": word
-           }
+@app.route("/api/v1/<word>/")
+def api(word):
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    result_dictionary = {"word": word, "definition": definition}
+    return result_dictionary
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
