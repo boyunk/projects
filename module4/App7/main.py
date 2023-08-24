df1fab371e7ec8d79ddb8803c9562322a5e68034
boyun <boyunk@uci.edu>
2023-08-24 13:38:43 -0700

    D32: GUI for the Weather Forecast Dashboard

---
+++ module4/App7/main.py
@@ -0,0 +1,14 @@
+#Building the GUI for Weather Forecaset Dashboard
+import streamlit as st
+
+st.title("Weather Forecast for the Next Days")
+# interactive widgits should have variables!
+place = st.text_input("Place:")
+days = st.slider("Forecast Days", min_value =1, max_value =5,
+                 help = "Select the number of days")
+
+option = st.selectbox("Select data to view",
+                      ("Temperature","Sky"))
+
+st.subheader(f"{option} for the next {days} days in {place}")
+
