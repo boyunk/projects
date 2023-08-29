0c9fd6693b43795ef666338b0c058efba4ea4467
boyun <boyunk@uci.edu>
2023-08-29 12:48:04 -0700

    D32: edited based on solution

--- module4/App7/Practice/main.py
+++ module4/App7/Practice/main.py
@@ -4,18 +4,39 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+# add title to widget
 st.title("In Search for happiness")
 
-df = pd.read_csv("App7/Practice/happy.csv")
 
+# add select boxes
 choice1 = st.selectbox("Select the data for X-axis",
                      ("GDP", "Happiness", "Generosity"))
 choice2 = st.selectbox("Select the data for Y-axis",
                      ("GDP", "Happiness", "Generosity"))
 
-x = df[f"{choice1}".lower()]
-y = df[f"{choice2}".lower()]
+# load data
+df = pd.read_csv("App7/Practice/happy.csv")
 
-figure = px.scatter(x=x, y=y,
-                    labels={"x":f"{choice1}","y":f"{choice2}"})
+# match the value of first option
+match choice1:
+    case "Happiness":
+        x_array = df["happiness"]
+    case "GDP":
+        x_array = df["gdp"]
+    case "GDP":
+        x_array = df["gdp"]
+
+# match the value of second option
+match choice2:
+    case "Happiness":
+        y_array = df["happiness"]
+    case "GDP":
+        y_array = df["gdp"]
+    case "GDP":
+        y_array = df["gdp"]
+
+#add subheader for the plot
+st.subheader(f"{choice1} and {choice2}")
+figure = px.scatter(x=x_array, y=y_array,
+                    labels={"x": choice1,"y": choice2})
 st.plotly_chart(figure)
