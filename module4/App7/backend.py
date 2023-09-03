da419ae151b2628cd37640c7e3bcf8b26c65319b
boyun <boyunk@uci.edu>
2023-09-03 11:26:37 -0700

    D33: front end sky element(key still does not work)

--- module4/App7/backend.py
+++ module4/App7/backend.py
@@ -10,16 +10,10 @@ def get_data(place, forecast_days, type=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        # use list comprehension
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-        # the reason for getting 0 is slightly confusing to me
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Seoul", forecast_days = 3, kind="Temperature"))
+    print(get_data(place="Seoul", forecast_days = 3))
 
 
 # example https://pro.openweathermap.org/data/2.5/forecast/climate?q=Seoul&appid=d2fbdd626f00504374736ac44b6fd28f
--- module4/App7/main.py
+++ module4/App7/main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add a title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
 # interactive widgits should have variables!
 place = st.text_input("Place:")
@@ -14,9 +15,37 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+# fix the error that appears when the place is not inserted
+    if place:
 
-d, t =get_data(days)
+    # get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y= t, labels={"x": "date", "y": "Temperature(F)"})
-st.plotly_chart(figure)
+    if option = "temperature":
+        # moving the filtering process to main to be processed once the temperature option is selected
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+        # list comprehension for date
+        dates = [dict["dt.txt"] for dict in filtered_data]
+        # create a temperature plot
+        figure = px.line(x=d, y= temperature, labels={"x": "date", "y": "Temperature(F)"})
+        st.plotly_chart(figure)
+
+
+    if option = "Sky":
+        # creating a dictionary for sky coniditions and images
+        # Option 1: use for loops
+        #sky_conditions = ["Clear", "Clouds", "Rain", "Snow"]
+        #sky_dict = {}
+        #for conditions in sky_conditions:
+        #    image_path = f"images/{sky_conditions}.png"
+        #    sky_dict[condition] = image_path
+
+        # Option 2: use list comprehension
+        sky = ["clear", "cloud", "rain", "snow"]
+        images = {condition: f"images/{condition}.png" for condition in sky}
+
+        # moving code from backend to front
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        print(sky_conditions)
+        st.image(image_paths, width = 115)
