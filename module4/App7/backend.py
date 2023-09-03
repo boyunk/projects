f7e9a656515cfad3190d154bb7b9c3d565acbdbd
boyun <boyunk@uci.edu>
2023-09-03 10:30:40 -0700

    D33: retreiving data from api (key does not work)

--- module4/App7/backend.py
+++ module4/App7/backend.py
@@ -3,14 +3,23 @@ import requests
 API_key="d2fbdd626f00504374736ac44b6fd28f"
 
 
-def get_data(place, forecast_days=None, type=None):
+def get_data(place, forecast_days, type=None):
     url = f"https://pro.openweathermap.org/data/2.5/forecast/climate?q={place}&appid={API_key}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        # use list comprehension
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+        # the reason for getting 0 is slightly confusing to me
+    return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place = "Seoul"))
+    print(get_data(place="Seoul", forecast_days = 3, kind="Temperature"))
 
 
 # example https://pro.openweathermap.org/data/2.5/forecast/climate?q=Seoul&appid=d2fbdd626f00504374736ac44b6fd28f
